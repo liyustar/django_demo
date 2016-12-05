001cf3c1b6318788e3a96d5ed515ef86baea620c
liyustar <liyustar@gmail.com>
2016-12-06 01:10:44 +0800

    first empty index

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+class IndexView(generic.TemplateView):
+    template_name = "demo/index.html"
+
+
+def index(request):
+    return render(request, "demo/index.html")
