c5911a87bfa1be66cb747a3d7f93e3736115c2b9
liyuxing <yuxing.li@ims.jsfund.cn>
2017-12-14 02:08:03 +0800

    修改模型

--- demo2/models.py
+++ demo2/models.py
@@ -6,15 +6,15 @@ class FundInfo(models.Model):
     id = models.AutoField(primary_key=True)
     symbol = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
-    wave3 = models.DecimalField(max_digits=18, decimal_places=6)
-    wave_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
-    risk3 = models.DecimalField(max_digits=18, decimal_places=6)
-    risk_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
-    sharp3 = models.DecimalField(max_digits=18, decimal_places=6)
-    sharp_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
-    return_rate = models.DecimalField(max_digits=18, decimal_places=6) # return
-    rank = models.IntegerField()
-    update_time = models.DateTimeField()
+    wave3 = models.FloatField(null=True)
+    wave_evaluate3 = models.FloatField(null=True)
+    risk3 = models.FloatField(null=True)
+    risk_evaluate3 = models.FloatField(null=True)
+    sharp3 = models.FloatField(null=True)
+    sharp_evaluate3 = models.FloatField(null=True)
+    return_rate = models.FloatField(null=True) # return
+    rank = models.IntegerField(null=True)
+    update_time = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.text
