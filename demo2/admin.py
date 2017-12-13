1db562a0f076be7133cc45a0cbaded777d93656b
liyuxing <yuxing.li@ims.jsfund.cn>
2017-12-14 01:47:48 +0800

    add FundInfo Model

--- demo2/admin.py
+++ demo2/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
+from .models import FundInfo
 
 # Register your models here.
-# class CounterAdmin(admin.ModelAdmin):
-#     list_display = ('text', 'count')
-#
-# admin.site.register(Counter, CounterAdmin)
+class FundInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'symbol', 'name', 'wave3', 'wave_evaluate3', 'risk3', 'risk_evaluate3', 'sharp3', 'sharp_evaluate3', 'return_rate', 'rank', 'update_time')
+
+admin.site.register(FundInfo, FundInfoAdmin)
--- demo2/models.py
+++ demo2/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-# class Counter(models.Model):
-#     text = models.CharField(max_length=200)
-#     count = models.IntegerField(default=0)
-#
-#     def __str__(self):
-#         return self.text
+class FundInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    symbol = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    wave3 = models.DecimalField(max_digits=18, decimal_places=6)
+    wave_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
+    risk3 = models.DecimalField(max_digits=18, decimal_places=6)
+    risk_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
+    sharp3 = models.DecimalField(max_digits=18, decimal_places=6)
+    sharp_evaluate3 = models.DecimalField(max_digits=18, decimal_places=6)
+    return_rate = models.DecimalField(max_digits=18, decimal_places=6) # return
+    rank = models.IntegerField()
+    update_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.text
